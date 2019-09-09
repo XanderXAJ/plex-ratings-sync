c5b2b015352713653a6e42a32f4ad7377334d439
Alex Palmer <xanderx@xanderx.com>
2019-09-09 08:32:24 +0100

    Allow --reset to be run standalone

--- cliparser.py
+++ cliparser.py
@@ -6,14 +6,18 @@ class CliParser:
 		super().__init__()
 
 		parser.add_argument('--config_file', default='config.ini')
-		parser.add_argument('--input_format', choices=['double_colon', 'google_play_json'], required=True)
+		parser.add_argument('--input_format', choices=['double_colon', 'google_play_json'])
 		parser.add_argument('--reset', action='store_true', default=False)
-		parser.add_argument('input_file')
+		parser.add_argument('input_file', nargs='?')
 
 		self._parser = parser
 
 	def parse(self, *args) -> argparse.Namespace:
 		if len(args) > 0:
-			return self._parser.parse_args(args)
+			values = self._parser.parse_args(args)
 		else:
-			return self._parser.parse_args()
+			values = self._parser.parse_args()
+
+		if (values.input_file is None or values.input_format is None) and values.reset is False:
+			self._parser.error('Need either input (both file and --input_format) or --reset')
+		return values
--- test_cliparser.py
+++ test_cliparser.py
@@ -22,6 +22,10 @@ class CliParserArgs(unittest.TestCase):
 		self.parser = CliParser(TestArgumentParser())
 		self.base_args = ['--input_format', 'double_colon', 'file']
 
+	def test_parses_input_file(self):
+		actual = self.parser.parse('--input_format', 'double_colon', 'file')
+		self.assertEqual(actual.input_file, 'file')
+
 	def test_default_config_file(self):
 		actual = self.parser.parse(*self.base_args)
 		self.assertEqual('config.ini', actual.config_file)
@@ -38,6 +42,15 @@ class CliParserArgs(unittest.TestCase):
 		with self.assertRaises(TestArgumentParserError):
 			self.parser.parse('file')
 
+	def test_reset_can_be_run_standalone(self):
+		actual = self.parser.parse('--reset')
+		self.assertTrue(actual.reset)
+
+	def test_reset_can_be_run_alongside_ratings_sync(self):
+		actual = self.parser.parse('--reset', '--input_format', 'double_colon', 'file')
+		self.assertTrue(actual.reset)
+		self.assertEqual(actual.input_format, 'double_colon')
+		self.assertEqual(actual.input_file, 'file')
 
 if __name__ == '__main__':
 	unittest.main()
